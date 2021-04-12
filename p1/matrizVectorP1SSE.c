2ec511b5076cc015c3b9e122b67059ebdaed2e1a
meluchoMZ <miguelgodon@outlook.es>
2021-04-12 23:28:06 +0200

    Added non _mm_hadd_ps version. Added comentaries. Little syntax changes

--- p1/matrizVectorP1SSE.c
+++ p1/matrizVectorP1SSE.c
@@ -6,15 +6,14 @@
 #include <sys/time.h>
 #include <pmmintrin.h>
 
-#define HADD
-#define NOHADD
+//#define HADD
 
 int main(int argc, char *argv[])
 {
 	int m, _m, n, _n, test, i, j, errors;
 	float alpha, *x, *A, *y, *testy;
 	struct timeval t0, t1, t;
-	__m128 alpha_reg, x_reg, a_reg[4];
+	__m128 alpha_reg, x_reg, a_reg[4], aux_unpack[5];
 
 	if (argc == 5) {
 		m = atoi(argv[1]);
@@ -26,20 +25,18 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	_m = m; _n = n;
-
-	if ((float) m/4 > (int) m/4) {
-		_m = (int) (m/4 + 1) * 4;
-	}
-	if ((float) n/4 > (int) n/4) {
-		_n = (int) (n/4 + 1) * 4;
-	}
+	// se os datos son múltiplos de 4, non se modifican na dimensión que sexan múltiplo
+	// noutro caso, amplíase a matriz ao seguinte múltiplo maís próximo
+	_m = ((float) m/4 > (int) m/4) ? ((int) (m/4+1)*4) : m;
+	_n = ((float) n/4 > (int) n/4) ? ((int) (n/4+1)*4) : n;
 	printf("_m = %d; _n = %d\n", _m, _n);
 
+	// resérvase memoria cun aliñamento de 16 bytes
 	x = (float *) _mm_malloc(_n*sizeof(float), 16);
 	A = (float *) _mm_malloc(_m*_n*sizeof(float), 16);
 	y = (float *) _mm_malloc(_m*sizeof(float), 16);
 
+	// inicialízanse os datos
 	for (i = 0; i < m; i++) {
 		for (j = 0; j < n; j++) {
 			A[i*_n+j] = 1+i+j;
@@ -84,23 +81,67 @@ int main(int argc, char *argv[])
 #ifdef HADD
 	for (i = 0; i < m; i+=4) {
 		for (j = 0; j < n; j+=4) {
+			// cárganse 16 bytes de x
 			x_reg = _mm_load_ps(&x[j]);
+			// multiplícanse polo factor de escalado
+			x_reg = _mm_mul_ps(x_reg, alpha_reg);
 			for (char c = 0x0; c < 0x4; c++) {
+				// carganse 16 bytes da matriz
 				a_reg[c] = _mm_load_ps(&A[(i+c)*_n+j]);
+				// multiplicación elemento e elemento
 				a_reg[c] = _mm_mul_ps(a_reg[c], x_reg);
 			}
+			// sumas horizontais dos vectores 2 a 2
 			a_reg[0] = _mm_hadd_ps(a_reg[0], a_reg[1]);
 			a_reg[2] = _mm_hadd_ps(a_reg[2], a_reg[3]);
 			a_reg[0] = _mm_hadd_ps(a_reg[0], a_reg[2]);
+			// cárgase o valor actual de y (neses 16 bytes)
 			a_reg[1] = _mm_load_ps(&y[i]);
+			// súmase o valor do produto matriz-vector a y
 			a_reg[0] = _mm_add_ps(a_reg[0], a_reg[1]);
+			// gárdase o valor no vector resultado
 			_mm_store_ps(&y[i], a_reg[0]);
 		}
 	}
 #else 
-	for (i = 0; i < m; i++) {
-		for (j = 0; j < n; j++) {
-			y[i] += alpha*A[i*n+j]*x[j];
+	for (i = 0; i < m; i+=4) {
+		for (j = 0; j < n; j+=4) {
+			// cárganse 16 bytes de x
+			x_reg = _mm_load_ps(&x[j]);
+			// multiplícanse polo factor de escalado
+			x_reg = _mm_mul_ps(x_reg, alpha_reg);
+			for (char c = 0x0; c < 0x4; c++) {
+				// cárganse 16 bytes da matriz
+				a_reg[c] = _mm_load_ps(&A[(i+c)*_n+j]);
+				// multiplícase a matriz por x
+				a_reg[c] = _mm_mul_ps(a_reg[c], x_reg);
+			}
+			// mestúranse os primeiros 8 bytes dos rexistros 
+			aux_unpack[0] = _mm_unpacklo_ps(a_reg[0], a_reg[2]);
+			aux_unpack[1] = _mm_unpacklo_ps(a_reg[1], a_reg[3]);
+			// mestúranse os primeiros 8 bytes dos rexistros, de xeito que 
+			// quedan os primeiros 4 bytes de cada a_reg[i] orixinal (a0,c0,b0,d0)
+			aux_unpack[2] = _mm_unpacklo_ps(aux_unpack[0], aux_unpack[1]);
+			// mestúranse so últimos 8 bytes dos rexistros, de xeito que quedan
+			// os segundos 4 bytes de cada a_reg[i] orixinal (a1,c1,b1,d1)
+			aux_unpack[3] = _mm_unpackhi_ps(aux_unpack[0], aux_unpack[1]);
+			// mestúranse os 8 bytes máis significativos dos rexistros
+			aux_unpack[0] = _mm_unpackhi_ps(a_reg[0], a_reg[2]);
+			aux_unpack[1] = _mm_unpackhi_ps(a_reg[1], a_reg[3]);
+			// mestúranse os primeiros 8 bytes dos rexistros, quedando os bytes
+			// 7 a 11 de cada a_reg[i] orixinal (a2,c2,b2,d2)
+			aux_unpack[4] = _mm_unpacklo_ps(aux_unpack[0], aux_unpack[1]);
+			// mestúranse os últimos 4 bytes de cada a_reg[i] orixinal (a3,c3,b3,d3)
+			aux_unpack[0] = _mm_unpackhi_ps(aux_unpack[0], aux_unpack[1]);
+			// súmanse verticalmente os vectores
+			aux_unpack[0] = _mm_add_ps(aux_unpack[4], aux_unpack[0]);
+			aux_unpack[1] = _mm_add_ps(aux_unpack[2], aux_unpack[3]);
+			aux_unpack[0] = _mm_add_ps(aux_unpack[0], aux_unpack[1]);
+			// engádese o valor a y
+			a_reg[0] = _mm_load_ps(&y[i]);
+			a_reg[0] = _mm_add_ps(a_reg[0], aux_unpack[0]);
+			// gárdase o valor no vector resultado
+			_mm_store_ps(&y[i], a_reg[0]);
 		}
 	}
 #endif
